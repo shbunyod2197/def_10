0ad64c13b2dfa9b95103f5a0834887551ae41729
Bunyod <shbunyod2197@gmail.com>
2026-02-27 14:43:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+# 1
+def juftmi(a,b):
+    return a == b
+
+a = juftmi(2,2)
+print(a)
+2
+def tengmi(a,b):
+    return a == b
+
+a = tengmi(2,2)
+print(a)
+# 3
+def matn(a):
+    return a.upper()
+
+a = matn("jlshslghs")
+print(a)
+# 4
+def matn(a):
+    return a.lower()
+
+a = matn("ayhug")
+print(a)
+# 5
+
+def lenn(a):
+    return len(a)
+a = lenn("jhasfkjaaaaaadgbzfsyhwgefgyewfbcyjadfsjafvgdjsjasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjjfffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffjfjfjfjfjfjfjfjfjfjfjfjjfjfjfjfjfjfjfjfjfjfjfjfjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfffffffffffffffffffffffffffffffjjjjjjjjjjfjjjjj")
+
+print(a)
+# 6
+
+def yigindi_3r(a, b, c):
+    return  a + b + c
+
+a = yigindi_3r(3,4,4)
+print(a)
+# 7
+def orqa_summ(s, b):
+    natija = (s + b) / 2
+    return natija
+
+a = orqa_summ(4, 8)
+print(a)
+8
+def len(a):
+    return len(a)
+
+a = len("jhasfkjaaaaaadgbzfsyhwgefgyewfbcyjadfsjafvgdjsjasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjjfffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffjfjfjfjfjfjfjfjfjfjfjfjjfjfjfjfjfjfjfjfjfjfjfjfjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfffffffffffffffffffffffffffffffjjjjjjjjjjfjjjjj")
+print(a)
+# 9
+def katta_r(a, b, c):
+    return max(a, b, c)
+
+a = katta_r(4, 8, 872358685462565635456674276247762674254625426)
+print(a)
+# 10
+def tabrik(a, b, c="Tabriklaymiz"):
+    return a, b, c
+
+a = tabrik("Sobr", "Jigar",)
+print(a)
